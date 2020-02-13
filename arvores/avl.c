3c59f49cf236d7dc4bd62eb461613751a0228a39
pedroailan <pedroaso@dcomp.ufs.br>
2020-02-12 21:47:28 -0300

    rotações: direita e esquerda

--- arvores/avl.c
+++ arvores/avl.c
@@ -6,7 +6,7 @@ typedef struct reg
     int info;
     struct reg *esq;
     struct reg *dir;
-
+    int altura;
 } Arv;
 
 Arv* criar_vazia()
@@ -19,12 +19,60 @@ int vazia(Arv* a)
     return a == NULL;
 }
 
+int height (Arv* a)
+{
+    if (a == NULL) return 0;
+    return a->altura;
+}
+
+int profundidade(Arv* a)
+{
+	if(a == NULL) return 0;
+	return(height(a->esq) - height(a->dir));
+}
+
+int maior(int a, int b)
+{
+	return (a > b)? a : b;
+}
+
+Arv* rotDireita(Arv* a)
+{
+	Arv* aux = a->esq;
+	Arv* auz = a->dir;
+
+	aux->dir = a;
+	a->esq = auz;
+
+	a->altura = maior(height(a->esq), height(a->dir)) + 1;
+	aux->altura = maior(height(aux->esq), height(aux->dir)) + 1;
+	return a;
+}
+
+Arv* rotEsquerda(Arv* a)
+{
+	Arv* aux = a->dir;
+	Arv* auz = aux->esq;
+
+	aux->esq = a;
+	a->dir = auz;
+
+	a->altura = maior(height(a->esq), height(a->dir))+1;
+	aux->altura = maior(height(aux->esq), height(aux->dir));
+
+	return a;
+}
+
+
+
+
 Arv* inserir(Arv* a, int c) 
 {
     if(a == NULL)
     {
         a = malloc(sizeof(Arv));
         a->info = c;
+        a->altura = 1;
         a->esq = a->dir = NULL;
     } else {
         if (c < a->info) 
@@ -35,6 +83,24 @@ Arv* inserir(Arv* a, int c)
         {
             a->dir = inserir(a->dir, c);
         }
+    }
+    return a;
+    a->altura = 1 + maior(height(a->esq), height(a->dir));
+
+    int balanco = profundidade(a);
+
+    if (balanco > 1 && c < a->esq->info) return rotDireita(a);
+    if (balanco < -1 && c > a->dir->info) return rotEsquerda(a);
+    if (balanco > 1 && c > a->esq->info)
+    {
+    	a->esq = rotEsquerda(a->esq);
+    	return rotDireita(a);
+    }
+
+    if(balanco < -1 && c < a->dir->info)
+    {
+    	a->dir = rotDireita(a->dir);
+    	return rotEsquerda(a);
     }
     return a;
 }
@@ -65,7 +131,5 @@ int main()
     imprime(a);
     printf("\n");
     
-    
     return 0;
 }
-
